a4fc96542c636b5f95bea5876cdd4c696d1b44c0
Aleksey Kochanov <dzot-1993@yandex.ru>
2022-12-06 17:27:27 +0300

    fix after code review

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 
 app_name = 'api'
 
-v1_router = routers.SimpleRouter()
+v1_router = routers.DefaultRouter()
 v1_router.register(r'users', UserViewSet, basename='users')
 v1_router.register(r'titles', TitleViewSet, basename='titles')
 v1_router.register(r'genres', GenreViewSet, basename='genres')
